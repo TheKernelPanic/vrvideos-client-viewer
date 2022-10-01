e4084a5cac2835846766c90df9a1e50d66fc8b89
KernelPanic <kernelpanic0x000007C4@protonmail.com>
2022-10-02 00:51:16 +0200

    Add hosts.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  applicationServer: {
+    host: 'http://192.168.1.131:8001'
+  },
+  cdnImages: {
+    host: 'http://192.168.1.131:8002'
+  }
 };
